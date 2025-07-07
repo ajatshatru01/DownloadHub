da0679c2439d9c64b9e88e6ff4c3e6fa7a6275ab
UTSAV OJHA <ajatsatru787@gmail.com>
2025-07-07 16:12:50 +0530

    Create appData.js

---
+++ appData.js
@@ -0,0 +1,123 @@
+const items = [
+    {
+      id: 1,
+      name: "Subway Surfers",
+      category: "apps",
+      downloadUrl: "files/sample-app.apk",
+      rating: 4.5,
+      description: "Run fast, dodge trains, and collect coins!",
+      image: "Subway_Surfers.jpg",
+      reviews: []
+    },
+    {
+      id: 2,
+      name: "Instagram",
+      category: "apps",
+      downloadUrl: "files/sample-app.apk",
+      rating: 4.2,
+      description: "Share your moments with friends.",
+      image: "Instagram_logo_2022.svg.png",
+      reviews: []
+    },
+    {
+      id: 3,
+      name: "Spotify",
+      category: "apps",
+      downloadUrl: "files/sample-app.apk",
+      rating: 4.7,
+      description: "Stream millions of songs anytime.",
+      image: "Spotify_icon.svg",
+      reviews: []
+    },
+    {
+        id: 4,
+        name: "Mini militia",
+        category: "apps",
+        downloadUrl: "files/sample-app.apk",
+        rating: 5,
+        description: "Mini Militia, commonly referred to as Mini Militia, is a 2D multiplayer action game developed by Appsomniacs LLC and published by Miniclip. The game was initially released for iOS in April 5, 2011 and later launched on Android in March 2015.",
+        image: "Mini.png",
+        reviews: []
+    },
+    {
+        id: 5,
+        name: "Mobile Legend",
+        category: "apps",
+        downloadUrl: "files/sample-app.apk",
+        rating: 4.2,
+        description: "Mobile Legends: Bang Bang is a mobile multiplayer online battle arena game developed and published by Chinese developer Moonton, a subsidiary of ByteDance. The game was released in 2016 and grew in popularity, most prominently in Southeast Asia.",
+        image: "mobile_legend.jpg",
+        reviews: []
+    },
+    {
+      id: 6,
+      name: "The Book Thief",
+      category: "books",
+      downloadUrl: "/files/Grade9GT--TheBookThiefMarkusZusak.pdf",
+      rating: 4.7,
+      description: "The Book Thief is a historical fiction novel by the Australian author Markus Zusak, set in Nazi Germany during World War II. ",
+      image: "book_thief.jpg",
+      reviews: []
+  },
+  {
+    id: 7 ,
+    name: "Srimad Bhagavad-Gita",
+    category: "books",
+    downloadUrl: "files/The Bhagavad Gita.pdf",
+    rating: 5,
+    description: "The Gita is the most sacred religious book. It is the literal word of Shree Krishna and holds complete knowledge of universe and spiritual growth. ",
+    image: "bhagavadgita-6.jpg",
+    reviews: []
+},
+{
+  id: 8 ,
+  name: "Puss in Boots: The Last Wish",
+  category: "movies",
+  downloadUrl: "files/sample-app.apk",
+  rating: 4.5,
+  description: "Puss in Boots: The Last Wish is a 2022 American animated adventure fantasy comedy film produced by DreamWorks Animation and distributed by Universal Pictures.",
+  image: "BOOM.jpg",
+  reviews: []
+},
+{
+  id: 9 ,
+  name: "The Kashmir Files",
+  category: "movies",
+  downloadUrl: "files/sample-app.apk",
+  rating: 5,
+  description: "Krishna endeavours to uncover the reason behind his parents' brutal killings in Kashmir. He is shocked to uncover a web of lies and conspiracies in connection with the massive genocide. ",
+  image: "Kashmir.jpg",
+  reviews: []
+},
+{
+  id: 10 ,
+  name: "Youtube",
+  category: "apps",
+  downloadUrl: "files/sample-app.apk",
+  rating: 5,
+  description: "YouTube is an American social media and online video sharing platform owned by Google.",
+  image: "images.png",
+  reviews: []
+},
+{
+  id: 11 ,
+  name: "Free-Fire",
+  category: "apps",
+  downloadUrl: "files/sample-app.apk",
+  rating: 4.7,
+  description: "Free Fire, formerly known as Garena Free Fire, is a free-to-play battle royale game developed and published by Garena for Android and iOS. ",
+  image: "im.jpg",
+  reviews: []
+},
+{
+  id: 12 ,
+  name: "GTA V",
+  category: "apps",
+  downloadUrl: "files/sample-app.apk",
+  rating: 5,
+  description: "Grand Theft Auto V is a 2013 action-adventure game developed by Rockstar North and published by Rockstar Games.",
+  image: "gta.webp",
+  reviews: []
+},
+  ];
+  
